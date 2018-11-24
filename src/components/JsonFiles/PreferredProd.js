67b6d394be1d46b1ed154c1a3bd63b6a1eb23eff
wiame <kazdari.wiam@gmail.com>
2018-11-24 04:23:10 +0100

    update the preferred list

--- src/components/JsonFiles/PreferredProd.js
+++ src/components/JsonFiles/PreferredProd.js
@@ -1,14 +1,19 @@
 var myPreferredProducts = [
-  {id:1, products:[
+  {id_user:1, products:[1,2,3]},
+  {id_user:2, products:[4,2,5]}
+]
+
+export default myPreferredProducts;
+
+/*var myPreferredProducts = [
+  {id_user:1, products:[
       {id: 1, label:'item1',  price: '$459.99', image:'item1'},
       {id: 2, label:'item2',  price: '$5000', image:'item2'},
       {id: 3, label:'item3',  price: '$11000', image:'item3'},
   ]},
-  {id:2, products:[
+  {id_user:2, products:[
       {id: 1, label:'item1',  price: '$459.99', image:'item1'},
       {id: 2, label:'item2',  price: '$5000', image:'item2'},
       {id: 3, label:'item3',  price: '$11000', image:'item3'},
   ]}
-]
-
-export default myPreferredProducts;
+]*/
